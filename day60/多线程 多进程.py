6db2bd3e325352708356d4ebd6dded52ef48b1b5
wx <813703110@qq.com>
2022-06-16 01:27:17 +0800

    多线程/进程、线程/进程池

---
+++ day60/多线程 多进程.py
@@ -0,0 +1,31 @@
+from threading import Thread
+from multiprocessing import Process
+
+
+# 多线程
+# 方法一：
+def func(name):
+    for i in range(1000):
+        print('func', i, name)
+
+# Process和Thread关键词互掉即可切换多进程和多线程
+if __name__ == '__main__':
+    t1 = Thread(target=func, args=('Jay',))
+    t1.start()
+    t2 = Thread(target=func, args=('Tony',))
+    t2.start()
+    for i in range(1000):
+        print('main', i)
+
+# 方法二：
+# class MyThread(Thread):
+#     def run(self):
+#         for i in range(1000):
+#             print('func', i)
+#
+#
+# if __name__ == '__main__':
+#     t = MyThread()
+#     t.start()
+#     for i in range(1000):
+#         print('main', i)
---
+++ day60/线程池 进程池.py
@@ -0,0 +1,17 @@
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+import time
+
+
+def func(name):
+    for i in range(1000):
+        print(i, name)
+
+
+if __name__ == '__main__':
+    start = time.time()
+    # with ThreadPoolExecutor(20) as t:  # 线程池 2.6s
+    with ProcessPoolExecutor(20) as t:  # 进程池 0.8s
+        for i in range(100):
+            t.submit(func, name='线程')
+    end = time.time()
+    print('done', end - start)
