4433ee43d45533048fe778c9e9ac17b24190ba18
wx <813703110@qq.com>
2022-06-30 17:05:57 +0800

    爬热搜

--- day75/爬itab热搜.py
+++ day75/爬itab热搜.py
@@ -4,11 +4,15 @@ import json
 
 url = 'https://api.codelife.cc/api/top/list'
 headers = {
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36 Edg/103.0.1264.37',
-
+    'accept': 'application/json, text/plain, */*',
+    'accept-encoding': 'gzip, deflate',
+    'accept-language': 'zh-CN,zh;q=0.9',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+    'referer': 'https://go.itab.link/',
+    'signaturekey': 'U2FsdGVkX1/rN/AZrL38LLGBt6iP85IQ5Ilk0QXls4A='
 }
 data = {
-    id: "mproPpoq6O"
+    id: "KqndgxeLl9"
 }
 resp = requests.post(url, data=data, headers=headers)
 
